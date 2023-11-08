cd0e4e387fcada47786c1312cbe3f20700c69384
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2023-11-09 05:53:16 +0800

    Create Bonezegei_LCD1602_I2C.h

---
+++ src/Bonezegei_LCD1602_I2C.h
@@ -0,0 +1,15 @@
+/*
+  This Library is written for HD44780 LCD with PCF8574 I2C IO expander
+  Author: Bonezegei (Jofel Batutay)
+  Date:  November 2023 
+*/
+#ifndef _BONEZEGEI_LCD1602_I2C_H_
+#define _BONEZEGEI_LCD1602_I2C_H_
+#include <Arduino.h>
+
+class Bonezegei_LCD1602_I2C {
+public:
+
+};
+
+#endif
