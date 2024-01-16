365174e52a697a2f0b0c1542d55b950c42185f7f
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-01-17 00:59:53 +0800

    Create HelloWorld.ino

---
+++ examples/HelloWorld/HelloWorld.ino
@@ -0,0 +1,18 @@
+/*
+  Hello World 
+  Author: Bonezegei (Jofel Batutay)
+  Date:  January 2024
+*/
+
+#include <Bonezegei_LCD1602_I2C.h>
+
+Bonezegei_LCD1602_I2C lcd(0x27);
+
+void setup() {
+  lcd.begin();
+
+}
+
+void loop() {
+
+}
