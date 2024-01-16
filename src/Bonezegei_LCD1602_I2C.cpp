61fcef8f2ba347d504b25f3865efcf11d7830d26
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-01-17 00:55:30 +0800

    Update Bonezegei_LCD1602_I2C.cpp

--- src/Bonezegei_LCD1602_I2C.cpp
+++ src/Bonezegei_LCD1602_I2C.cpp
@@ -1,25 +1,25 @@
 /*
-  This Library is written for HD44780 LCD with PCF8574 I2C IO expander
+  This Library is written for PCF8574 I2C Connected to HD44780 16x2 LCD
   Author: Bonezegei (Jofel Batutay)
-  Date:  November 2023 
+  Date:  January 2024
 */
 
 #include "Bonezegei_LCD1602_I2C.h"
 
-Bonezegei_LCD1602_I2C::Bonezegei_LCD1602_I2C(){
-  
+Bonezegei_LCD1602_I2C::Bonezegei_LCD1602_I2C() {
 }
 
-Bonezegei_LCD1602_I2C::Bonezegei_LCD1602_I2C(uint8_t addr){
+Bonezegei_LCD1602_I2C::Bonezegei_LCD1602_I2C(uint8_t addr) {
   _addr = addr;
 }
 
-char Bonezegei_LCD1602_I2C::begin(){
-  if ( pcf8574.begin() ) {
+char Bonezegei_LCD1602_I2C::begin() {
+  if (Wire.begin()) {
     return 1;
-  }
-  else {
+  } else {
     return 0;
   }
-  
+}
+
+void Bonezegei_LCD1602_I2C::write(uint8_t data) {
 }
