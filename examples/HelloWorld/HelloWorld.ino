addd4e7acd9e55dee20e8ccccde65af2b87db103
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-01-17 10:46:26 +0800

    Update HelloWorld.ino

--- examples/HelloWorld/HelloWorld.ino
+++ examples/HelloWorld/HelloWorld.ino
@@ -10,9 +10,18 @@ Bonezegei_LCD1602_I2C lcd(0x27);
 
 void setup() {
   lcd.begin();
-
+  lcd.print("Bonezegei");
+  lcd.setPosition(0, 1);      //param1 = X   param2 = Y
+  lcd.print("HD44780");
+  delay(2000);
+  lcd.setPosition(0, 0);
+  lcd.print("Hello World");
 }
 
 void loop() {
-
+  lcd.setPosition(0, 1);
+  String str = "ms:";
+  str += millis();
+  lcd.print(str.c_str());
+  delay(100);
 }
