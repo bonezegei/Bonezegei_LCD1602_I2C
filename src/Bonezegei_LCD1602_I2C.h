d50cddc0ab8dc4994b0aa48ae91dc946ccb68442
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-01-17 10:47:36 +0800

    Update Bonezegei_LCD1602_I2C.h

--- src/Bonezegei_LCD1602_I2C.h
+++ src/Bonezegei_LCD1602_I2C.h
@@ -16,7 +16,6 @@
 
 #ifndef _BONEZEGEI_LCD1602_I2C_H_
 #define _BONEZEGEI_LCD1602_I2C_H_
-
 #include <Arduino.h>
 #include <Wire.h>
 
@@ -26,8 +25,25 @@ public:
   Bonezegei_LCD1602_I2C(uint8_t addr);
 
   char begin();
+  
+  //I2C write
   void write(uint8_t data);
 
+  //clock effect
+  void latch();
+
+  // set the last nibble to the Data pins
+  void write4Bit(uint8_t data);
+  void writeByte(uint8_t data);
+
+  void writeCommand(uint8_t data);
+  void writeData(uint8_t data);
+
+  void setPosition(uint8_t x, uint8_t y);
+  void print(const char *str);
+
+  void setBacklight(uint8_t data);
+  
   uint8_t _addr;
   uint8_t p_write;
 };
